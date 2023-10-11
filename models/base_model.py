7e08fd468570a151e67b1cbbd2bbdf617fe18486
254jack <taixonj@gmail.com>
2023-10-11 12:02:54 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,20 +1,41 @@
+#!/usr/bin/python3
+
+"""
+BaseModel module
+"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
+    """
+    Base class with all common methods for other classes
+    """
+
     def __init__(self):
+        """
+        instatiates objects with it's attributes
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """ 
+        Returns the string representation of the instance
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Updates the public instance attribute updated_at with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns a dictionary containing all values/keys of __dict__ of the instance
+        """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
         obj_dict["created_at"] = self.created_at.isoformat()
