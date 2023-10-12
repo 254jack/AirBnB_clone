5e20e02253e33f22200715d7b3dd899215335da2
254jack <taixonj@gmail.com>
2023-10-12 13:12:23 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,10 +12,19 @@ class BaseModel:
     Base class with all common methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         instatiates objects with it's attributes
         """
+        if len(kwargs) > 0:
+            for key, value in kwargs.items():
+                if key == '__class__':
+                    continue
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.fromisoformat(value)
+                setattr(self, key, value)
+            return
+        
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
