0892d864f7afe1c774d59215bdba1b4d753ff1b1
254jack <taixonj@gmail.com>
2023-10-14 21:38:03 +0300

    base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -10,7 +10,7 @@ from datetime import datetime
 
 class BaseModel:
     """
-    Base class with all common methods for other classes
+    BaseModels class
     """
 
     def __init__(self, *args, **kwargs):
@@ -25,7 +25,7 @@ class BaseModel:
                     value = datetime.fromisoformat(value)
                 setattr(self, key, value)
             return
-        
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
@@ -40,14 +40,14 @@ class BaseModel:
 
     def save(self):
         """
-        Updates the public instance attribute updated_at with the current datetime
+        Updates the `updated_at` with the current datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all values/keys of __dict__ of the instance
+        Returns a dictionary containing all values of __dict__ of the instance
         """
         obj_dict = self.__dict__.copy()
         obj_dict["__class__"] = self.__class__.__name__
