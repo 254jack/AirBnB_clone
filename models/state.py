1a5500725236e78f46f323374d07f3dc22e8aeed
254jack <taixonj@gmail.com>
2023-10-14 21:45:21 +0300

    state.py

--- models/state.py
+++ models/state.py
@@ -1,16 +1,10 @@
 #!/usr/bin/python3
-"""The `state` module
-
-It defines one class, `State(),
-which sub-classes the `BaseModel()` class.`
+"""`state` module
 """
 from models.base_model import BaseModel
 
 
 class State(BaseModel):
-    """A state in the application.
-
-    Attributes:
-        name
+    """state class inheriting from BaseModel
     """
     name = ""
