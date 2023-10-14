fdd40c942df708107ab12ac77675fac59a3a0bcf
254jack <taixonj@gmail.com>
2023-10-14 21:39:55 +0300

    city.py

--- models/city.py
+++ models/city.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""The `city` module
-
-It defines one class, `City(),
-which sub-classes the `BaseModel()` class.`
+"""`city` module
 """
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """A city in the application.
-
-    Attributes:
-        name
-        state_id
+    """City class inheriting from BaseModel
     """
     name = ""
     state_id = ""
