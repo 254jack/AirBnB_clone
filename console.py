999db48b9daba52369c370ee24be8551c45698d9
254jack <taixonj@gmail.com>
2023-10-14 10:58:50 +0300

    console.py

---
+++ console.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    """Simple command processor example."""
+    prompt = "(hbnb)"
+    
+    def emptyline(self):
+        pass
+    def do_quit(self, line):
+        exit()
+    def help_quit(self):
+        print("Quit command to exit the program")
+    def help_EOF(self):
+        print("The end of file that exits the loop at the end of a file read")
+    def do_EOF(self, line):
+        return True
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+    
